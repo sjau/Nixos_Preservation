96188d0a2b90c9bc7f3780872498195b92582e4c
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2026-07-23 14:03:48 +0200

    Update lowRamInstall.sh

--- lowRamInstall.sh
+++ lowRamInstall.sh
@@ -1,19 +1,21 @@
 # To install in low ram environments we first juts use Disko for partitioning
-# Then we activated the zfs swap
-# We set the nix store to the zfs nix dataset
+# Then we create a swap file to expand the nix store in the memory
 # Finally we do the installation
 
 # Use Disko for partioning
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko disko.nix
 
-# Activate swap
-swapon -a /dev/zvol/tankTest/Nixos/encZFS/v/swap
-
 # Use the zfs tmp dataset as /tmp
 mount --bind /mnt/tmp /tmp
 
-# Set /nix/store
-#export NIX_STORE_DIR=/mnt/nix/store
+# Create swap file on /mnt/tmp - we will empty /tmp anyway on reboot...
+dd if=/dev/zero of=/mnt/tmp/swapfile bs=1M count=8192
+chmod 600 /mnt/tmp/swapfile
+mkswap /mnt/tmp/swapfile
+swapon /mnt/tmp/swapfile
+
+# Expand the overlay nix store
+mount -o remount,size=8G /nix/.rw-store
 
 # Install Nixos
-#nixos-install --flake .#nixos
+nixos-install --flake .#nixos
