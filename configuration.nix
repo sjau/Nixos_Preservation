e2dd69b5b8ff1c441b6563d96281d244a019e48a
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2026-07-24 05:03:49 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -47,9 +47,6 @@
         hostKeys = [
             {   path = "/data/ssh/ssh_host_ed25519_key";
                 type = "ed25519"; }
-            {   path = "/data/ssh/ssh_host_rsa_key";
-                type = "rsa";
-                bits = 4096; }
         ];
         settings = {
             PermitRootLogin = "yes";
